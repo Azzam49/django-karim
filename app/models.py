77513e2bb2ca9e74e081ca088475c689a2013286
Azzam49 <azzam49coder@gmail.com>
2025-09-27 18:36:31 +0800

    Added age field

--- app/models.py
+++ app/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Students(models.Model):
-    name = models.CharField(max_length=50)
+    # https://docs.google.com/spreadsheets/d/1HbxbGMj871pJwRKxQ-kIwWLWhZHm0flN8v9e0GeCYEc/edit?usp=sharing
+    name = models.CharField(max_length=50) # required
+    age = models.IntegerField(blank=True, null=True) # optional
