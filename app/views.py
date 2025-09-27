234a38c1cc51d3d93d566f59ed82df6f50ae0bd5
Azzam49 <azzam49coder@gmail.com>
2025-09-27 19:45:50 +0800

    Creating our firt page at '/'

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import Students
+
+def home(request):
+    # ORM : Object Relational Mapping
+    # ORM Query : Students.objects.all()
+    # SQL Query : SELECT * FROM app_students
+    students = Students.objects.all()
+
+    print(f"\n\nstudents : {students}\n\n")
+    print(f"\n\nstudents type : {type(students)}\n\n")
+
+    
