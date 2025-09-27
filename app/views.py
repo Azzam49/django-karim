254d1faacdf8afea568ac9908cbed7b99cc340cb
Azzam49 <azzam49coder@gmail.com>
2025-09-27 20:07:22 +0800

    Using data on our home.html page

--- app/views.py
+++ app/views.py
@@ -8,8 +8,39 @@ def home(request):
     # ORM Query : Students.objects.all()
     # SQL Query : SELECT * FROM app_students
     students = Students.objects.all()
+    # print(f"\n\nstudents : {students}\n\n")
+    # print(f"\n\nstudents type : {type(students)}\n\n")
 
-    print(f"\n\nstudents : {students}\n\n")
-    print(f"\n\nstudents type : {type(students)}\n\n")
 
-    return render(request, "app/home.html", {})
+    # students = [
+    #     {
+    #         "id": 1,
+    #         "name": "Adam",
+    #         "age": 10,
+    #     },
+    #     {
+    #         "id": 1,
+    #         "name": "Jane",
+    #         "age": 15,
+    #     },
+    #     {
+    #         "id": 1,
+    #         "name": "Mike",
+    #         "age": 20,
+    #     }
+    # ]
+
+
+    # for student in students:
+    #     # Printing our testing Dict. : print(f"Name: {student['name']} | Age : {student['age']}")
+    #     # Printing our QuerySet:
+    #     print(f"Name: {student.name} | Age : {student.age}")
+
+    # context is a common naming for the dict that django sends to the html file
+    # this dict will contains all the data that html page shall uses.
+    context = {
+        "title": "Our Students:",
+        "students": students,
+    }
+
+    return render(request, "app/home.html", context)
